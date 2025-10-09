356799e64b7bcd2cf8f3d3c20a3bfb7f00ed2265
임진수 <jsbd0109@naver.com>
2025-10-09 16:54:33 +0900

    decrease-and-conquer

---
+++ week6/fac.py
@@ -0,0 +1,16 @@
+# 하향식 축소 정복
+def fac1(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fac1(n-1)
+
+# 상향식 축소 정복
+def fac2(n):
+    result = 1
+    for k in range(1, n+1):
+        result *= k
+    return result
+
+print(fac1(5))
+print(fac2(5))
