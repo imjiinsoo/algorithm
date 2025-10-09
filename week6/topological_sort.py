01eb35c8cb805c294832e8ef1d291d03f3fabc78
임진수 <jsbd0109@naver.com>
2025-10-09 18:07:27 +0900

    topological_sort

---
+++ week6/topological_sort.py
@@ -0,0 +1,35 @@
+# 위상 정렬
+def topological_sort(graph):
+    inDeg = {}
+    for v in graph:
+        inDeg[v] = 0
+    for v in graph:
+        for u in graph[v]:
+            inDeg[u] += 1
+
+    vlist = []
+    for v in graph:
+        if inDeg[v] == 0:
+            vlist.append(v)
+
+    while vlist:
+        v = vlist.pop()
+        print(v, end=' ')
+
+        for u in graph[v]:
+            inDeg[u] -= 1
+            if inDeg[u] == 0:
+                vlist.append(u)
+
+# 연결되어 있는 것들
+mygraph = { "A" : {"C", "D"},
+            "B" : {"D", "E"},
+            "C" : {"D", "F"},
+            "D" : {"F"},
+            "E" : {"F"},
+            "F" : set()
+            }
+
+print("topological_sort: ")
+topological_sort(mygraph)
+print()
