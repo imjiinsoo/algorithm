5983df7b2c9e94549b88958f892070d107f65099
임진수 <jsbd0109@naver.com>
2025-11-14 10:11:55 +0900

    fibo_table 추가

--- week11/fibo.py
+++ week11/fibo.py
@@ -13,12 +13,24 @@ def fibo_mem(n):
             mem[n] = fibo_mem(n-1) + fibo_mem(n-2)
     return mem[n]
 
-mem = [None] * 100
+# 테이블화
+def fibo_table(n):
+    f = [None] * 100
+    f[0] = 0
+    f[1] = 1
+    for i in range(2, n+1):
+        f[i] = f[i-1] + f[i-2]
+    return f[n]
 
+mem = [None] * 100
 # 같은 문제를 반복해서 계산하기 때문에 비효율적임
 print("분할정복 방식: ", end='')
 print(fibo(9))
 
-# 시/공간 복잡도가 n에서 끝남
+# 하향식 방법, 시/공간 복잡도가 n에서 끝남
 print("메모제이션 방식: ", end='')
 print(fibo_mem(9))
+
+# 상향식 방법
+print("테이블화 방식: ", end='')
+print(fibo_table(9))
