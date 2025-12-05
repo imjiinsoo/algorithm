64dbf758ab6ff7e37433b9763f517635f4ad297b
임진수 <jsbd0109@naver.com>
2025-12-05 10:48:37 +0900

    N-Queen

---
+++ week14/N-Queen.py
@@ -0,0 +1,38 @@
+def isSafe(board,x,y):
+    N = len(board)
+
+    for i in range(y):
+        if board[i][x] == 1: return False # 세로방향 검사
+    for i,j in zip(range(y-1,-1,-1), range(x-1,-1,-1)):
+        if board[i][j] == 1: return False # \방향 검사
+    for i, j in zip(range(y - 1, -1, -1), range(x+1, N)):
+        if board[i][j] == 1: return False # /방향 검사
+    return True # 모든 방향이 ok면 (x,y)는 safe한 위치
+
+def solve_N_Queen(board, y):
+    N = len(board)
+    if y == N:
+        printBoard(board)
+        return
+
+    for x in range(N):
+        if isSafe(board,x,y):
+            board[y][x]=1
+            solve_N_Queen(board, y+1)
+            board[y][x] = 0
+
+
+def printBoard(board):
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                print("Q", end=" ")
+            else:
+                print(".", end=" ")
+        print()
+    print()
+
+
+N = 4
+board = [[0 for i in range(N)] for j in range(N)]
+solve_N_Queen(board, 0)
